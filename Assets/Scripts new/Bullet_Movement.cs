8253e712762b577a9b6f40d03c3056bc87bad6f6
ODRI-the-human <orangedingo2@gmail.com>
2022-08-14 20:46:35 +0930

    bouncy was fucked but now it is just fixed

--- Assets/Scripts new/Bullet_Movement.cs
+++ Assets/Scripts new/Bullet_Movement.cs
@@ -20,31 +20,24 @@ public class Bullet_Movement : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D col)
     {
-        if (gameObject.GetComponent<ItemBOUNCY>() == null && gameObject.GetComponent<ItemPIERCING>() == null)
+        if (gameObject.GetComponent<ItemPIERCING>() != null && gameObject.GetComponent<ItemPIERCING>().piercesLeft > 0)
         {
-            Destroy(gameObject);
+            Physics2D.IgnoreCollision(col.gameObject.GetComponent<Collider2D>(), gameObject.GetComponent<Collider2D>(), true);
+            rb.velocity = OriginalSpeed;
+            gameObject.GetComponent<ItemPIERCING>().piercesLeft--;
         }
         else
         {
-            if (gameObject.GetComponent<ItemPIERCING>().piercesLeft > 0)
+            if (gameObject.GetComponent<ItemBOUNCY>() != null && gameObject.GetComponent<ItemBOUNCY>().bouncesLeft > 0)
             {
-                Physics2D.IgnoreCollision(col.gameObject.GetComponent<Collider2D>(), gameObject.GetComponent<Collider2D>(), true);
-                rb.velocity = OriginalSpeed;
-                gameObject.GetComponent<ItemPIERCING>().piercesLeft--;
+                Vector2 enemyPos = new Vector2(transform.position.x, transform.position.y);
+                Vector2 bulletPos = new Vector2(col.transform.position.x, col.transform.position.y);
+                rb.velocity = speed * (enemyPos - bulletPos).normalized;
+                gameObject.GetComponent<ItemBOUNCY>().bouncesLeft--;
             }
             else
             {
-                if (gameObject.GetComponent<ItemBOUNCY>().bouncesLeft > 0)
-                {
-                    Vector2 enemyPos = new Vector2(transform.position.x, transform.position.y);
-                    Vector2 bulletPos = new Vector2(col.transform.position.x, col.transform.position.y);
-                    rb.velocity = speed * (enemyPos - bulletPos).normalized;
-                    gameObject.GetComponent<ItemBOUNCY>().bouncesLeft--;
-                }
-                else
-                {
-                    Destroy(gameObject);
-                }
+                Destroy(gameObject);
             }
         }
     }
