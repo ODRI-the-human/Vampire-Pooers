897de3e7fd8cffcca49d3734e1a82bd9be921208
ODRI-the-human <orangedingo2@gmail.com>
2022-08-14 21:04:43 +0930

    item pedestal improvement
    two item pedestals can't have the same item anymore.

--- Assets/Scripts new/itemPedestal.cs
+++ Assets/Scripts new/itemPedestal.cs
@@ -7,12 +7,30 @@ public class itemPedestal : MonoBehaviour
     public int itemChosen;
     public Sprite[] spriteArray;
     public SpriteRenderer spriteRenderer;
+    bool isFine = false;
+    float minRange = -0.5f;
+    float maxRange = 16.5f; // should be 22.5f for all current items okie
+    GameObject[] gos;
 
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
-        itemChosen = Mathf.RoundToInt(Random.Range(-0.5f, 16.5f)); // should be 22.5f for all current items okie
+        itemChosen = Mathf.RoundToInt(Random.Range(minRange, maxRange));
         spriteRenderer.sprite = spriteArray[itemChosen];
+        gos = GameObject.FindGameObjectsWithTag("item");
+    }
+
+    void Update()
+    {
+        foreach (GameObject go in gos)
+        {
+            if (go.GetComponent<itemPedestal>().itemChosen == itemChosen && go != gameObject)
+            {
+                Debug.Log("WOw, something was fucked up!!!!!!!!!!!!");
+                itemChosen = Mathf.RoundToInt(Random.Range(minRange, maxRange));
+                spriteRenderer.sprite = spriteArray[itemChosen];
+            }
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col)
