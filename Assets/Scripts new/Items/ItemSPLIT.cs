d684ce7c96fc240945c781e0e094d08037541d00
ODRI-the-human <orangedingo2@gmail.com>
2023-01-01 02:25:22 +1030

    better splitty
    Split shots now don't collide with the enemy that was the conduit for their spawning, and, ya know, work properly.

--- Assets/Scripts new/Items/ItemSPLIT.cs
+++ Assets/Scripts new/Items/ItemSPLIT.cs
@@ -14,53 +14,38 @@ public class ItemSPLIT : MonoBehaviour
 
     void Start()
     {
-        speed = gameObject.GetComponent<Bullet_Movement>().speed;
-
-        if (gameObject.tag == "PlayerBullet")
-        {
-            Buuleter = GameObject.Find("bigFuckingMasterObject").GetComponent<EntityReferencerGuy>().playerBullet;
-        }
-        else
-        {
-            Buuleter = GameObject.Find("bigFuckingMasterObject").GetComponent<EntityReferencerGuy>().enemyBullet;
-        }
+        owner = gameObject.GetComponent<DealDamage>().owner;
+        speed = gameObject.GetComponent<Rigidbody2D>().velocity.magnitude;
+        Buuleter = owner.GetComponent<Attack>().Bullet;
     }
+
+
 
     // Start is called before the first frame update
     void OnCollisionEnter2D(Collision2D col)
     {
         Vector2 enemyPos = new Vector2(transform.position.x, transform.position.y);
         Vector2 bulletPos = new Vector2(col.transform.position.x, col.transform.position.y);
+        ShotVector = speed * (bulletPos - enemyPos).normalized;
 
         if (canSplit && col.gameObject.tag != "Wall")
         {
             if (gameObject.tag == "PlayerBullet" || gameObject.tag == "enemyBullet")
             {
-                ShotVector = speed * (bulletPos - enemyPos).normalized;
-                GameObject Splitman1 = Instantiate(Buuleter, transform.position, transform.rotation);
-                Splitman1.transform.localScale = 0.5f * gameObject.transform.localScale;
-                Splitman1.GetComponent<DealDamage>().finalDamageMult *= 0.3f * gameObject.GetComponent<DealDamage>().finalDamageMult * instances;
-                Splitman1.GetComponent<DealDamage>().knockBackCoeff = 0.5f * gameObject.GetComponent<DealDamage>().knockBackCoeff;
-                owner = gameObject.GetComponent<DealDamage>().owner;
-                Splitman1.GetComponent<DealDamage>().owner = owner;
-                Splitman1.GetComponent<DealDamage>().damageBase += owner.GetComponent<Attack>().Crongus;
-                Splitman1.AddComponent<ItemSPLIT>();
-                Splitman1.GetComponent<ItemSPLIT>().canSplit = false;
-                Splitman1.GetComponent<ItemHolder>().itemsHeld = gameObject.GetComponent<ItemHolder>().itemsHeld;
-                bulletRB = Splitman1.GetComponent<Rigidbody2D>();
-                bulletRB.velocity = new Vector3(ShotVector.x * Mathf.Cos(-Mathf.PI / 2) - ShotVector.y * Mathf.Sin(-Mathf.PI / 2), ShotVector.x * Mathf.Sin(-Mathf.PI / 2) + ShotVector.y * Mathf.Cos(-Mathf.PI / 2), 0);
-                GameObject Splitman2 = Instantiate(Buuleter, transform.position, transform.rotation);
-                Splitman2.transform.localScale = 0.5f * gameObject.transform.localScale;
-                Splitman2.GetComponent<DealDamage>().finalDamageMult *= 0.3f * gameObject.GetComponent<DealDamage>().finalDamageMult * instances;
-                Splitman2.GetComponent<DealDamage>().knockBackCoeff = 0.5f * gameObject.GetComponent<DealDamage>().knockBackCoeff;
-                owner = gameObject.GetComponent<DealDamage>().owner;
-                Splitman2.GetComponent<DealDamage>().owner = owner;
-                Splitman2.GetComponent<DealDamage>().damageBase += owner.GetComponent<Attack>().Crongus;
-                Splitman2.AddComponent<ItemSPLIT>();
-                Splitman2.GetComponent<ItemSPLIT>().canSplit = false;
-                Splitman2.GetComponent<ItemHolder>().itemsHeld = gameObject.GetComponent<ItemHolder>().itemsHeld;
-                bulletRB = Splitman2.GetComponent<Rigidbody2D>();
-                bulletRB.velocity = new Vector3(ShotVector.x * Mathf.Cos(Mathf.PI / 2) - ShotVector.y * Mathf.Sin(Mathf.PI / 2), ShotVector.x * Mathf.Sin(Mathf.PI / 2) + ShotVector.y * Mathf.Cos(Mathf.PI / 2), 0);
+                for (int i = 0; i < 2; i++)
+                {
+                    GameObject Splitman1 = Instantiate(Buuleter, transform.position, transform.rotation);
+                    Splitman1.transform.localScale = 0.3f * instances * gameObject.transform.localScale;
+                    Splitman1.GetComponent<DealDamage>().finalDamageMult *= 0.3f * gameObject.GetComponent<DealDamage>().finalDamageMult * instances;
+                    Splitman1.GetComponent<DealDamage>().knockBackCoeff = 0.5f * gameObject.GetComponent<DealDamage>().knockBackCoeff;
+                    Splitman1.GetComponent<DealDamage>().owner = owner;
+                    Splitman1.GetComponent<DealDamage>().damageBase += owner.GetComponent<Attack>().Crongus;
+                    Splitman1.GetComponent<Rigidbody2D>().simulated = true;
+                    Splitman1.GetComponent<DealDamage>().isBulletClone = true;
+                    Splitman1.GetComponent<Rigidbody2D>().velocity = new Vector3(ShotVector.x * Mathf.Cos(-((i * 2) - 1) * Mathf.PI / 2) - ShotVector.y * Mathf.Sin(-((i * 2) - 1) * Mathf.PI / 2), ShotVector.x * Mathf.Sin(-((i * 2) - 1) * Mathf.PI / 2) + ShotVector.y * Mathf.Cos(-((i * 2) - 1) * Mathf.PI / 2), 0);
+                    Physics2D.IgnoreCollision(col.gameObject.GetComponent<Collider2D>(), Splitman1.GetComponent<Collider2D>(), true);
+                    Splitman1.GetComponent<ItemSPLIT>().canSplit = false;
+                }
             }
         }
     }
